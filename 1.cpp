671331954e05753ef7d8c43ec16b9133ab5cbf79
Thanhtran1808 <93958877+Thanhtran1808@users.noreply.github.com>
2021-11-09 14:07:19 +0700

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,12 @@
+#include "stdio.h"
+#include "conio.h"
+int main()
+{
+char ch;
+if ((ch>='a') && (ch<='z')){
+	printf("Chu cai in thuong");
+} else if (((ch>='A') && (ch<='Z'))){
+	printf("Chu cai in hoa");
+} 
+getch();
+}
